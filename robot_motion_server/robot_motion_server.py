aa4e4cee9a762bb5b9bd6c1e2ecf1246574816aa
sushanth <sushanth.jayanth@gmail.com>
2023-11-07 20:14:35 -0500

    minimizing only x_distance

--- robot_motion_server/robot_motion_server.py
+++ robot_motion_server/robot_motion_server.py
@@ -51,7 +51,7 @@ class DockingUndockingActionServer(Node):
         self.robot_pose = None
         self.distance_to_goal = None
         self.goal_pose = None
-        self.goal_threshold = 0.265
+        self.goal_threshold = 0.255
 
         # construct the action server
         self._action_server = ActionServer(
