f9e5bb6a3176bacbfe76dc69cc0c373cc4b91470
sushanth <sushanth.jayanth@gmail.com>
2023-11-17 15:31:34 -0500

    in progress

--- robot_motion_server/robot_motion_server.py
+++ robot_motion_server/robot_motion_server.py
@@ -43,20 +43,24 @@ class DockingUndockingActionServer(Node):
             action_server_name = "/" + robot_namespace + "/" + "DockUndock"
             publisher_topic = "/" + robot_namespace + "/cmd_vel"
             subscriber_topic = "/" + robot_namespace + "/map_pose"
-            velocity_subscriber_topic = "/" + robot_namespace + "/odom"
         else:
             action_server_name = "DockUndock"
             publisher_topic = "/cmd_vel"
             subscriber_topic = "/map_pose"
-            velocity_subscriber_topic = "/odom"
 
         self.get_logger().info(f"cmd vel topic is {publisher_topic}")
         self.robot_pose = None
-        self.robot_vel = None
+        self.fiducial_range = None
+        self.fiducial_orientation = None
+        self.fiducial_lateral_offset = None
         self.goal_pose = None
-        self.goal_pose_2 = None
-        self.goal_threshold = 0.07
-        self.undock_duration = 2
+        self.goal_lateral_threshold = 0.05 # metres
+        self.robot_to_fiducial_goal_distance = 0.75 # metres
+        self.undock_duration = 1
+
+        # define biases to be used for docking
+        self.x_bias = 0.0
+        self.y_bias = 0.0
 
         # construct the action server
         self._action_server = ActionServer(
@@ -68,7 +72,7 @@ class DockingUndockingActionServer(Node):
 
         self.publisher_ = self.create_publisher(Twist, publisher_topic, 10)
 
-        # Pose Subscribers
+        # Pose Subscriber
         self.pose_subscription = self.create_subscription(
             msg_type=PoseWithCovarianceStamped,
             topic=subscriber_topic,  # Topic on which pose is being relayed
@@ -76,17 +80,45 @@ class DockingUndockingActionServer(Node):
             qos_profile=10,  # Adjust the queue size as needed
         )
 
-        # Velocity Subscribers
-        self.velocity_subscription = self.create_subscription(
-            msg_type=Odometry,
-            topic=velocity_subscriber_topic,  # Topic on which pose is being relayed
-            callback=self.robot_velocity_callback,
+        # Fiducial Subscriber
+        self.fiducial_subscription = self.create_subscription(
+            msg_type="", #TODO # Add message type
+            topic="/fiducial_marker_data",
+            callback=self.fiducial_orientation_range_callback,
             qos_profile=10,  # Adjust the queue size as needed
         )
 
         # get euclidean distance first
         self.define_goal_pose()
-        self.y_delta = None
+
+    def define_goal_pose(self, dx, dy):
+        self.goal_pose = PoseStamped()
+        self.goal_pose.pose.position.x = self.robot_pose.pose.pose.position.x + dx
+        self.goal_pose.pose.position.y = self.robot_pose.pose.pose.position.y + dy
+        self.goal_pose.pose.position.z = 0.0
+        self.goal_pose.pose.orientation.x = 0.0
+        self.goal_pose.pose.orientation.y = 0.0
+        self.goal_pose.pose.orientation.z = self.robot_pose.pose.pose.orientation.z
+        self.goal_pose.pose.orientation.w = self.robot_pose.pose.pose.orientation.w
+
+    def robot_pose_callback(self, msg : PoseWithCovarianceStamped):
+        self.robot_pose = msg
+
+    def fiducial_orientation_range_callback(self, msg):
+        self.fiducial_lateral_offset = msg + self.y_bias #TODO use the correct attribute of msg like msg.range
+        self.fiducial_range = msg #TODO use the correct attribute of msg like msg.range
+        self.fiducial_orientation = msg #TODO use the correct attribute of msg like msg.range
+
+    def publish_zero_twist(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        self.publisher_.publish(msg)
+
+    def publish_action_server_result(self, flag, goal_handle):
+        goal_handle.succeed()
+        result = DockUndock.Result()
+        result.success = flag
+        return result
 
     def wrap_around_pi(self, x):
         x = abs(x)
@@ -98,74 +130,47 @@ class DockingUndockingActionServer(Node):
     def radians_to_degrees(self, angle):
         return angle * 180.0 / 3.1415926
 
-    def calc_dx_dy_dtheta(self, pose1):
+    def calc_dtheta(self):
+        pose1 = self.goal_pose
         pose2 = self.robot_pose
+
         # Extract the positions from the poses
         if isinstance(pose1, PoseStamped) and isinstance(pose2, PoseWithCovarianceStamped):
             pos1 = pose1.pose
             pos2 = pose2.pose.pose
-        elif pose1 is None: # in Navigation State
+        elif pose1 is None:
             return None
         else:
             raise ValueError("Input pose1 is not a valid type (Pose or PoseWithCovariance)")
 
-        # Calculate the Euclidean distance
-        # dx = goal_pose - robot_pose
-        dx = pos1.position.x - pos2.position.x
-        dy = pos1.position.y - pos2.position.y
-        # Calculate the orientation error (theta_error) using arctangent
-        theta_error = math.atan2(dy, dx)
         # Convert orientations to Euler angles (roll, pitch, and yaw)
         euler_angles1 = euler_from_quaternion([pos1.orientation.x, pos1.orientation.y, pos1.orientation.z, pos1.orientation.w])
         euler_angles2 = euler_from_quaternion([pos2.orientation.x, pos2.orientation.y, pos2.orientation.z, pos2.orientation.w])
         # Calculate the orientation difference
-        true_theta_error = self.wrap_around_pi(euler_angles2[2] - euler_angles1[2])
+        dtheta = self.radians_to_degrees(self.wrap_around_pi(euler_angles2[2] - euler_angles1[2]))
+        return dtheta # dtheta in degrees
 
-        return dx, dy, theta_error, true_theta_error
-
-
-    def get_individual_orientations(self, pose1):
+    def calc_euclidean_dist(self):
+        pose1 = self.goal_pose
         pose2 = self.robot_pose
+
         # Extract the positions from the poses
         if isinstance(pose1, PoseStamped) and isinstance(pose2, PoseWithCovarianceStamped):
             pos1 = pose1.pose
             pos2 = pose2.pose.pose
-        elif pose1 is None: # in Navigation State
+        elif pose1 is None:
             return None
         else:
             raise ValueError("Input pose1 is not a valid type (Pose or PoseWithCovariance)")
+        dx = pos1.position.x - pos2.position.x
+        dy = pos1.position.y - pos2.position.y
 
-        # Convert orientations to Euler angles (roll, pitch, and yaw)
-        euler_angles1 = euler_from_quaternion([pos1.orientation.x, pos1.orientation.y, pos1.orientation.z, pos1.orientation.w])
-        euler_angles2 = euler_from_quaternion([pos2.orientation.x, pos2.orientation.y, pos2.orientation.z, pos2.orientation.w])
+        return math.sqrt(dx**2 + dy**2)
 
-        return euler_angles1, euler_angles2
-
-
-    def world_frame_orientation(self):
-        orientation = self.robot_pose.pose.pose.orientation
-        euler_angle_rad = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
-        return euler_angle_rad[2]
-
-    def define_goal_pose(self):
-        self.goal_pose = PoseStamped()
-        self.goal_pose.pose.position.x = 6.658
-        self.goal_pose.pose.position.y = -0.1
-        self.goal_pose.pose.position.z = 0.0
-        self.goal_pose.pose.orientation.x = 0.0
-        self.goal_pose.pose.orientation.y = 0.0
-        self.goal_pose.pose.orientation.z = -0.675927815
-        self.goal_pose.pose.orientation.w = 0.7369678335
-
-
-    def robot_pose_callback(self, msg):
-        self.robot_pose = msg
-
-    def robot_velocity_callback(self, msg):
-        self.robot_vel = msg.twist.twist
 
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing Docking/Undocking...')
+        result = DockUndock.Result()
 
         # Define and fill the feedback message
         # feedback_msg = DockUndock.Feedback()
@@ -179,7 +184,7 @@ class DockingUndockingActionServer(Node):
         dock_id = int(abs(goal_handle.request.secs))
         self.get_logger().info(f"Docking Goal is {goal_handle.request.secs}")
 
-        # Simple Undocking
+        ################## Simple Undocking ##################
         if goal_handle.request.secs > 0.0:
             msg = Twist()
             msg.linear.x = 0.2
@@ -189,34 +194,84 @@ class DockingUndockingActionServer(Node):
                 time.sleep(1)
 
             self.get_logger().info("FINISHED UNDOCKING")
-            result = DockUndock.Result()
             result.success = True
             goal_handle.succeed()
             return result
 
+        ################## Fiducial Docking ##################
+        # Verify if fiducial pose present
+        if self.fiducial_pose is None:
+            result.success = False
+            goal_handle.succeed()
+            return result
 
-        # Find direction of dock (Find along which axis is the dock)
-        dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(PoseStamped())
-        dock_axis = "y" if (true_dtheta > 0.8) else "x"
-        self.get_logger().info(f"dock axis is {dock_axis}")
-        # Figure out which direction robot needs to turn for 1st correction (1st while loop)
-        robot_orientation_in_world_frame = self.world_frame_orientation()
+        # Step1: Align Pose to fiducial
+        while(abs(self.fiducial_orientation) > 3): # NOTE: self.fiducial_orientation in degrees
+            rotation_direction = math.copysign(1, self.fiducial_orientation)
+            self.get_logger().info(f"Docking_1 in Progress with orientation {self.fiducial_orientation}")
+            msg = Twist()
+            msg.angular.z = 0.4 * rotation_direction
+            self.publisher_.publish(msg)
+            time.sleep(0.1)
 
-        dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-        if dock_axis == "x":
-            if abs(robot_orientation_in_world_frame) < 0.5:
-                rotation_direction = -1 if dy < 0.0 else 1
-            else:
-                rotation_direction = 1 if dy < 0.0 else -1
-        elif dock_axis == "y":
-            if (robot_orientation_in_world_frame  < -1) and (robot_orientation_in_world_frame  > -2):
-                self.get_logger().info("1st if")
-                rotation_direction = -1 if dx < 0.0 else 1
-            elif (robot_orientation_in_world_frame  > 1) and (robot_orientation_in_world_frame  < 2):
-                self.get_logger().info("2nd if")
-                rotation_direction = 1 if dx < 0.0 else -1
+        self.publish_zero_twist()
 
-        assert rotation_direction
+        # Step2: Calculate the error in pre_dock and use that to set new goal_pose
+        dy = self.fiducial_lateral_offset
+        dx = 0.0
+        self.define_goal_pose(dx, dy)
+
+        # Step3: Turn robot 90 degrees depending on which side of the fiducial we are located
+        rotation_direction_1 = math.copysign(1, dy)
+        rotation_direction_2 = -1 * math.copysign(1, dy)
+        dtheta = self.calc_dtheta()
+        while(not (dtheta > 44 and dtheta < 46)):
+            msg = Twist()
+            msg.angular.z = 0.4 * rotation_direction_1
+            self.publisher_.publish(msg)
+            time.sleep(0.1)
+            dtheta = self.calc_dtheta()
+
+        self.publish_zero_twist()
+
+        # Step4: Move to goal pose
+        dist = self.calc_euclidean_dist()
+        while(dist > self.goal_lateral_threshold):
+            msg = Twist()
+            msg.linear.x = 0.2
+            self.publisher_.publish(msg)
+            time.sleep(0.1)
+            dist = self.calc_euclidean_dist()
+
+        self.publish_zero_twist()
+
+        # Step5: Use the rotation_direction of Step3
+        dtheta = self.calc_dtheta()
+        while(dtheta < 2):
+            msg = Twist()
+            msg.angular.z = 0.4 * rotation_direction_2
+            self.publisher_.publish(msg)
+            time.sleep(0.1)
+            dtheta = self.calc_dtheta()
+
+        self.publish_zero_twist()
+
+        # Step6: Move robot backwards until fiducial range is minimized
+        while(self.fiducial_range > self.robot_to_fiducial_goal_distance):
+            msg = Twist()
+            msg.linear.x = -0.2
+            self.publisher_.publish(msg)
+            time.sleep(0.1)
+
+        # Step7: Verify if goal pose has been reached
+        if self.fiducial_lateral_offset < 0.05 and abs(self.fiducial_range - self.robot_to_fiducial_goal_distance) < 0.05:
+            self.get_logger().info(f"Successfully doced with fiducial lateral offset
+                                     at {self.fiducial_lateral_offset} and
+                                     fiducial range at {self.fiducial_range}")
+        else:
+            self.get_logger().info(f"Bad Docking with fiducial lateral offset
+                                     at {self.fiducial_lateral_offset} and
+                                     fiducial range at {self.fiducial_range}")
 
         # while(True):
         #     # dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
@@ -229,49 +284,6 @@ class DockingUndockingActionServer(Node):
         #     self.get_logger().info(f"rotation direction {rotation_direction}")
         #     time.sleep(10)
 
-        self.get_logger().info(f"true_dtheta is {true_dtheta}")
-        while(not (abs(true_dtheta) > 1.56 and abs(true_dtheta) < 1.58)):
-            self.get_logger().info("Docking_1 in Progress")
-            msg = Twist()
-            msg.angular.z = 0.4 * rotation_direction
-            self.publisher_.publish(msg)
-            dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-            self.get_logger().info(f"dx, dy, dtheta, and true_dtheta is {dx}, {dy}, {dtheta}, {true_dtheta}")
-            time.sleep(0.02)
-
-        dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-        stop_condition = lambda dist: abs(dist) > 0.05
-        while stop_condition(dy if dock_axis=="x" else dx):
-            self.get_logger().info("Docking_2 in Progress")
-            msg = Twist()
-            msg.linear.x = 0.4 # note this has to be positive due the first loop's reorientation
-            self.publisher_.publish(msg)
-            dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-            self.get_logger().info(f"dx, dy, dtheta, and true_dtheta is {dx}, {dy}, {dtheta}, {true_dtheta}")
-            time.sleep(0.02)
-
-        dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-        self.get_logger().info(f"true_dtheta is {true_dtheta}")
-        while(abs(true_dtheta) > 0.05):
-            self.get_logger().info("Docking_3 in Progress")
-            msg = Twist()
-            msg.angular.z = 0.4 * rotation_direction * -1
-            self.publisher_.publish(msg)
-            dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-            self.get_logger().info(f"dx, dy, dtheta, and true_dtheta is {dx}, {dy}, {dtheta}, {true_dtheta}")
-            time.sleep(0.02)
-
-        dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-        stop_condition = lambda dist: dist > 0.03
-        while stop_condition(dx if dock_axis=="x" else dy):
-            self.get_logger().info("Docking_4 in Progress")
-            msg = Twist()
-            msg.linear.x = -0.2
-            self.publisher_.publish(msg)
-            dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-            self.get_logger().info(f"dx, dy, dtheta, and true_dtheta is {dx}, {dy}, {dtheta}, {true_dtheta}")
-            time.sleep(0.02)
-
         msg = Twist()
         self.publisher_.publish(msg)
         self.get_logger().info("FINISHED DOCKING")
@@ -281,32 +293,6 @@ class DockingUndockingActionServer(Node):
         goal_handle.succeed()
 
         return result
-
-
-        #! Partially works
-        # prev_dx, prev_dtheta = 0.0, 0.0
-        # dx,dy,dtheta,true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-        # while(abs(dtheta) > 0.06 or abs(dy > 0.02)):
-        #     self.get_logger().info("Docking_1 in Progress")
-        #     ###### PID LOGIC ######
-        #     msg = Twist()
-        #     dx, dy, dtheta, true_dtheta = self.calc_dx_dy_dtheta(self.goal_pose)
-        #     # Kp_x = 0.3
-        #     Kp_theta = 0.4
-        #     Kd = 1.0
-        #     dt = 0.2
-        #     # delta_accel_x = Kp_x*dx + Kd*(dx - prev_dx)
-        #     delta_angular = Kp_theta*dtheta + Kd*(dtheta - prev_dtheta)
-        #     # vel_x = delta_accel_x * dt + self.robot_vel.linear.x
-        #     ang_vel_z = delta_angular * dt + self.robot_vel.angular.z
-        #     msg.linear.x = 0.2
-        #     msg.angular.z = ang_vel_z if true_dtheta < math.pi/2 else -ang_vel_z
-        #     self.publisher_.publish(msg)
-        #     # prev_dx = dx
-        #     prev_dtheta = dtheta
-        #     #######################
-        #     self.get_logger().info(f"dx, dy, dtheta, and true_dtheta is {dx}, {dy}, {dtheta}, {true_dtheta}")
-        #     time.sleep(0.2)
 
 
 class MotionActionServer(Node):
